720ce11b5cdfd995cef36d17df534ec3af6a5957
Ismail Opatola <opatolamails@gmail.com>
2021-09-29 05:38:31 +0100

    Update Info response
    Change content-type to text/html
    Send markup response

--- proxy-server-demo/proxy-server/index.js
+++ proxy-server-demo/proxy-server/index.js
@@ -1,11 +1,22 @@
 /**
- * Note
+ * Summary
  * ----
  * This Proxy is an intermediary application.
- * It sits between client and the two backend services: books and authors.
- * It processes/modifies the requests and responses in both directions.
  *
- * Uses cases
+ * Features:
+ * + It sits between the client and two backend services; books and authors.
+ * + It processes/modifies the requests and responses in both directions.
+ * + The Application is capable of monitoring http request and response.
+ *
+ * Currently captured metrics:
+ * + Duration of HTTP requests in microseconds
+ * + Total request number
+ * + Response time of last request
+ *
+ * Captured metrics are exposed at `localhost://8000/metrics` endpoint.
+ * This allows the use of Prometheus and Grafana service to store and visualize metrics.
+ *
+ * Proxy use-case:
  * Authorization - Forward only authorized requests to access a service.
  * Load balancing - Distribute inbound requests traffic equally among instances.
  * Logging - Log every requests going to a Back End API service.
@@ -38,7 +49,7 @@ const SUPPORTED_HTTP_METHODS = ["GET"];
 /**
  * Logging:
  * ---
- * Logs the incoming requests.
+ * Logs the incoming requests during developement.
  */
 app.use(morgan("dev"));
 
@@ -56,13 +67,15 @@ app.use("", (req, res, next) => {
 });
 
 /**
+ * HTTP Method Ristriction
+ * ---
  * Allow only supported methods
  */
 app.use("", (req, res, next) => {
   if (SUPPORTED_HTTP_METHODS.includes(req.method)) {
     next();
   } else {
-    res.sendStatus(400);
+    res.sendStatus(405);
   }
 });
 
@@ -82,7 +95,8 @@ register.setDefaultLabels({
 // client.collectDefaultMetrics({ register });
 
 /**
- * httpRequestDurationMicroseconds -
+ * httpRequestDurationMicroseconds
+ * ---
  * This histogram metric measures request duration under 10 seconds
  */
 const httpRequestDurationMicroseconds = new client.Histogram({
@@ -91,17 +105,21 @@ const httpRequestDurationMicroseconds = new client.Histogram({
   labelNames: ["method", "route", "code", "ip"],
   buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10],
 });
+
 /**
- * totalHttpRequestCount -
- * This Counter metric measures total request count made to for a each route
+ * totalHttpRequestCount
+ * ---
+ * This Counter metric measures total request count made to a each route
  */
 const totalHttpRequestCount = new client.Counter({
   name: "http_total_count",
   help: "total request number",
   labelNames: ["method", "path"],
 });
+
 /**
- * totalHttpRequestDuration -
+ * totalHttpRequestDuration
+ * ---
  * This Counter metric measures response time of last request
  */
 const totalHttpRequestDuration = new client.Gauge({
@@ -110,9 +128,21 @@ const totalHttpRequestDuration = new client.Gauge({
   labelNames: ["method", "path"],
 });
 
-// Registers multiple registries on the same endpoint
+/**
+ * Registers multiple registries on the same endpoint
+ * i.e [totalHttpRequestDuration,
+ * totalHttpRequestCount,
+ * httpRequestDurationMicroseconds]
+ */
 const mergedRegistries = client.Registry.merge([register, client.register]);
 
+/**
+ * Custom Middelware
+ * ---
+ * Captures httpRequestDurationMicroseconds
+ * Captures totalHttpRequestCount
+ * Captures totalHttpRequestDuration
+ */
 app.use("", (req, res, next) => {
   // Start the timer
   const end = httpRequestDurationMicroseconds.startTimer();
@@ -121,6 +151,7 @@ app.use("", (req, res, next) => {
   const route = req.url;
   const ip = req.ip;
 
+  // listen for finished response event and execute callback
   res.on("finish", () => {
     // count number of request
     totalHttpRequestCount.labels(req.method, route).inc();
@@ -140,9 +171,13 @@ app.use("", (req, res, next) => {
 // ---------------
 
 /**
- * Proxy all requests starting with "/books" to the books-server.
- * Define a pathRewrite so that /books is omitted when forwarded to the books API
- * Ensure the books server is live.
+ * Proxy Middleware - Books service
+ * ---
+ * + Proxy all requests starting with "/books" to the books-server.
+ * + Define a pathRewrite so that /books is re-written as "/"
+ * when forwarded to the books API.
+ *
+ * Requirement: Ensure the books-server is live.
  */
 app.use(
   "/books",
@@ -156,11 +191,14 @@ app.use(
 );
 
 /**
- * Proxy all requests starting with "/authors" to the authors-server.
- * Define a pathRewrite so that /authors is omitted when forwarded to the authors API
- * Ensure the authors server is live.
+ * Proxy Middleware - Authors service
+ * ---
+ * + Proxy all requests starting with "/authors" to the authors-server.
+ * + Define a pathRewrite so that /authors is re-written as "/"
+ * when forwarded to the authors API
+ *
+ * Requirement: Ensure the authors-server is live.
  */
-
 app.use(
   "/authors",
   createProxyMiddleware({
@@ -173,23 +211,65 @@ app.use(
 );
 
 /**
- * Info GET endpoint
+ * Info - GET endpoint
  * ---
  * responds with text information about the proxy server
  */
-// @ts-ignore
-app.get("/info", (req, res, next) => {
-  res.send("This is a proxy service which proxies to Books and Authors APIs.");
+app.get(["/", "/info"], (req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(
+    `<p>This is a proxy service which proxies to Books and Authors APIs.</p> 
+    <p><b>API Details:</b></p>
+    <table>
+      <thead>
+        <th>No.</th>
+        <th>Title</th>
+        <th>Method</th>
+        <th>Endpoint</th>
+      </thead>
+      <tbody>
+      <tr>
+        <td>1.</td>
+        <td>Authors</td>
+        <td>GET</td>
+        <td><a href="/authors">localhost://8000/authors</a></td>
+      </tr>
+      <tr>
+        <td>2.</td>
+        <td>Books</td>
+        <td>GET</td>
+        <td><a href="/books">localhost://8000/books</a></td>
+      </tr>
+      <tr>
+        <td>3.</td>
+        <td>Metrics</td>
+        <td>GET</td>
+        <td><a href="/metrics">localhost://8000/metrics</a></td>
+      </tr>
+      <tr>
+        <td>4.</td>
+        <td>Info</td>
+        <td>GET</td>
+        <td><a href="/info">localhost://8000/info</a></td>
+      </tr>
+      </tbody>
+    </table>`
+  );
 });
 
-// @ts-ignore
+/**
+ * Metrics - GET endpoint
+ * ---
+ * Get all registerd metrics.
+ * This API can be consumed by Prometheus.
+ */
 app.get("/metrics", async (req, res) => {
   // Return all metrics the Prometheus exposition format
   res.setHeader("Content-Type", register.contentType);
   res.end(await mergedRegistries.metrics());
 });
 
-// Start the Proxy
+// Start the Proxy Server
 app.listen(PORT, HOST, () => {
   console.log(`Proxy server is running on http://${HOST}:${PORT}`);
 });
