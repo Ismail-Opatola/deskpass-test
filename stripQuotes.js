497cd708672a2f2056245eab2b328e1e0f89b1c4
Ismail Opatola <opatolamails@gmail.com>
2022-04-01 03:59:41 +0100

    Create stripQuotes

--- stripQuotes.js
+++ stripQuotes.js
@@ -1,19 +1,19 @@
 // @ts-nocheck
 /**
  * Remove block quotes from string
- * @param {string} str 
+ * @param {string} str
  * @returns {string}
- * 
- * The function checks for single or double quotes character 
- * at the beginnig and end of a given string 
+ *
+ * The function checks for single or double quotes character
+ * at the beginnig and end of a given string
  * and replace the character with an empty string
- * 
- * The function uses Javascript string `replaceAll` method with 
- * a regular expression literal as it 1st argument and 
+ *
+ * The function uses Javascript string `replaceAll` method with
+ * a regular expression literal as it 1st argument and
  * a replacer string (empty string) as 2nd argument
- * 
+ *
  * ----
- * 
+ *
  * Regular expression literal consist of a pattern enclosed between slashes,
  * * `/` Open. Indecates the start of a regular expression,
  * * `^` matches the beginning of the string
@@ -24,13 +24,29 @@
  * * `+` match 1 or more of the preceding token.
  * * `/` Close. Indecates the end of a regular expression and the start of expression flags
  * * `g` is the global search flag.
- * 
+ *
  * Implementation:
  * ---------------
- * Replace all single/double quote occurrence with an empty string, 
+ * Replace all single/double quote occurrence with an empty string,
  * where `^['|"]` i.e "string begins with single quote or double quote"
  * or where `(['|"]$)` i.e "string ends with single quote or double quote"
+ *
+ * node backward compatibility, avoid using `replaceAll` instead use `replace` with a global flag
  */
 
-const removeBlockQuoteChar = (str = "") => str.replaceAll(/^['|"]|(['|"]$)+/g, '');
+const stripQuotes = (str = "", removeAll = false) => {
+  if (removeAll)
+    // strip all single and double quotes except `'s` apostrophe-s
+    return str.replace(
+      /([",']+)([",'']+)|[']+(?=[a-r|t-z])|(^['|"])|(['|"]$)+/g,
+      ""
+    );
 
+  // strip boundary quotes
+  return str.replace(/^['|"]|(['|"]$)+/g, "");
+};
+
+console.log(stripQuotes("'This '' is somebody's timeline'"));
+console.log(stripQuotes("'This '' is somebody's timeline'", true));
+console.log(stripQuotes("''Your pasta's so 'tasty'''", true));
+console.log(stripQuotes("''Your pasta's so ''tasty'''", true));
